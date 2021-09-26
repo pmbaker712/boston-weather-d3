a29bea490b306f18770ede1270e87edf3f95b85e
pmbaker712 <90076125+pmbaker712@users.noreply.github.com>
2021-09-26 11:35:27 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const fs = require('fs');
+
+const api_key = process.env['API_KEY'];
+
+// Visual Crossing weather API urls
+const historyUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Boston/last90days?key=' + api_key;
+
+const forecastUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Boston?unitGroup=us&key=' + api_key;
+
+const currentUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?aggregateHours=24&contentType=json&unitGroup=us&locationMode=single&key=' + api_key + '&locations=Boston';
+
+const app = express();
+
+// Load middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Domain whitelist for client apps
+app.use((req, res, next) => {
+    const whitelist = [
+        'https://cdpn.io',
+        'https://pmbaker712.github.io'
+    ];
+    if (whitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+
+    next();
+});
+
+
+app.listen(3000, () => {
+  console.log('Listening!');
+});
+
+function saveJson(url, file) {
+
+  request(url, (error, res, body) => {
+    if (error) {
+      return console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+
+      fs.writeFileSync(file + '.json', body);
+      console.log(file + '.json saved');
+    };
+  });
+
+  setInterval(function() {
+    request(url, (error, res, body) => {
+      if (error) {
+        return console.log(error)
+      };
+
+      if (!error && res.statusCode == 200) {
+
+        fs.writeFileSync(file + '.json', body);
+        console.log(file + '.json saved');
+      };
+    });
+  }, 15 * 60000);
+
+};
+
+saveJson(historyUrl, 'history');
+saveJson(forecastUrl, 'forecast');
+saveJson(currentUrl, 'current');
+
+function serveJson(file) {
+
+  app.get('/' + file, (req, res) => {
+
+    fs.readFile('./' + file + '.json', 'utf8', (err, data) => {
+
+      res.json(JSON.parse(data));
+      console.log(file + '.json sent')
+
+    });
+
+  });
+};
+
+serveJson('history');
+serveJson('forecast');
+serveJson('current');
+
+// API root endpoint
+app.get('/', (req, res) => {
+
+  res.sendFile(__dirname + '/public/api.html');
+
+});
