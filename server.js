e6ba32f278d38a1952e2f7f38f01ef590e5f2b02
Phillip Baker <90076125+pmbaker712@users.noreply.github.com>
2021-09-27 22:33:49 -0400

    Update server.js
    Added timestamp to console log.

--- server.js
+++ server.js
@@ -15,56 +15,69 @@ const currentUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices
 
 const app = express();
 
+function timeStamp() {
+
+  let dateObj = new Date(Date.now());
+
+  return dateObj.getMonth()
+    + '-' + dateObj.getDate()
+    + '-' + dateObj.getFullYear()
+    + ' ' + dateObj.getHours()
+    + ':' + dateObj.getMinutes()
+    + ' GMT';
+
+};
+
 // Load middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 // Domain whitelist for client apps
 app.use((req, res, next) => {
-    const whitelist = [
-        'https://cdpn.io',
-        'https://pmbaker712.github.io'
-    ];
-    if (whitelist.indexOf(req.headers.origin) !== -1) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    }
+  const whitelist = [
+    'https://cdpn.io',
+    'https://pmbaker712.github.io'
+  ];
+  if (whitelist.indexOf(req.headers.origin) !== -1) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  }
 
-    next();
+  next();
 });
 
 // Listen on port 3000
 app.listen(3000, () => {
-    console.log('Listening!');
+  console.log('Listening!');
 });
 
 // Get and store data from Visual Crossing API on a 15-minute interval
 function saveJson(url, file) {
 
-    function getJson() {
+  function getJson() {
 
-        request(url, (error, res, body) => {
-            if (error) {
-                return console.log(error)
-            };
+    request(url, (error, res, body) => {
+      if (error) {
+        return console.log(error)
+      };
 
-            if (!error && res.statusCode == 200) {
+      if (!error && res.statusCode == 200) {
 
-                fs.writeFileSync(file + '.json', body);
-                console.log(file + '.json saved');
-            };
-        });
-    };
+        fs.writeFileSync(file + '.json', body);
+        console.log(file + '.json saved' + ' ' + timeStamp());
+      };
+    });
+  };
 
-    // Get and save data immediately
+  // Get and save data immediately
+  getJson();
+
+  // Get and save data every 15 minutes
+  setInterval(function() {
     getJson();
-
-    // Get and save data every 15 minutes
-    setInterval(function() {
-        getJson();
-    }, 15 * 60000);
+  }, 15 * 60000);
 
 };
 
@@ -75,14 +88,14 @@ saveJson(currentUrl, 'current');
 // Serve data to client apps
 function serveJson(file) {
 
-    app.get('/' + file, (req, res) => {
+  app.get('/' + file, (req, res) => {
 
-        fs.readFile('./' + file + '.json', 'utf8', (err, data) => {
+    fs.readFile('./' + file + '.json', 'utf8', (err, data) => {
 
-            res.json(JSON.parse(data));
-            console.log(file + '.json sent')
-        });
+      res.json(JSON.parse(data));
+      console.log(file + '.json sent' + ' ' + timeStamp())
     });
+  });
 };
 
 serveJson('history');
@@ -92,6 +105,6 @@ serveJson('current');
 // API root endpoint
 app.get('/', (req, res) => {
 
-    res.sendFile(__dirname + '/public/api.html');
+  res.sendFile(__dirname + '/public/api.html');
 
 });
